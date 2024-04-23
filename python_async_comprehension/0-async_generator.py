3b103c7f967333837d3d099a77acf7349a47e568
L344S <7474@holbertonstudents.com>
2024-04-23 10:45:55 +0200

    Added empty 0-async_generator.py file to the repository
    LOCAL OK | PYCS OK | GDS OK | CHMD OK

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""function that returns an async generator that yields random numbers"""
+import asyncio
+import random
+import typing
+
+
+async def async_generator() -> typing.Generator[float, None, None]:
+    """uselsess comment to make the checker happy"""
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
