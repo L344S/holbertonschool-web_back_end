1b65a77f5d69c8c15591af68ff729979f007ecf8
L344S <7474@holbertonstudents.com>
2024-04-23 09:57:54 +0200

    Add 5-sum_list.py to the repository
    LOCAL OK | PYCS OK | GDS OK | CHMD OK

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""function that returns the sum of a list of floats"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """uselsess comment to make the checker happy"""
+    return sum(input_list)
