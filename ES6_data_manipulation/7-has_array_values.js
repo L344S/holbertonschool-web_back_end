4dec6ac4d2798d191abf534d29ad815b9a4f35cf
L344S <145486211+L344S@users.noreply.github.com>
2024-05-17 10:12:29 +0200

    FIX : Refactor hasValuesFromArray function to use array.every()

--- ES6_data_manipulation/7-has_array_values.js
+++ ES6_data_manipulation/7-has_array_values.js
@@ -1,9 +1,3 @@
 export default function hasValuesFromArray(set, array) {
-  let flag = true;
-  for (const set of array) {
-    if (set.has(set) === false) {
-      flag = false;
-    }
-  }
-  return flag;
+  return array.every((el) => set.has(el));
 }
